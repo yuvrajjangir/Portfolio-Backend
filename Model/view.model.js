3c899b3f6dae0075909997ef331aea8b5df4f846
yuvrajjangir <yuvijangir78@gmail.com>
2024-04-23 13:07:53 +0530

    done

--- Model/view.model.js
+++ Model/view.model.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 
 const viewsSchema = new mongoose.Schema({
-    count: { type: Number, default: 0 }
+    ip: { type: String, required: true },
+    count: { type: Number, default: 0 },
+    visited: { type: Boolean, default: false }
 });
 
 const Views = mongoose.model('Views', viewsSchema);
--- Routes/view.routes.js
+++ Routes/view.routes.js
@@ -1,14 +1,26 @@
-const {Router} = require("express");
+const { Router } = require("express");
 const { Views } = require("../Model/view.model");
 const viewsController = Router();
 
+// POST request to increment view count
 viewsController.post('/views', async (req, res) => {
     try {
-        let views = await Views.findOne();
-        if (!views) {
-            views = new Views();
+        const ipAddress = req.connection.remoteAddress;
+        if (!ipAddress) {
+            return res.status(400).send('Invalid IP address');
         }
-        views.count++;
+
+        let views = await Views.findOne({ ip: ipAddress });
+
+        if (!views) {
+            // If the IP address is not found, create a new entry
+            views = new Views({ ip: ipAddress, visited: true, count: 1 });
+        } else if (!views.visited) {
+            // If the IP address has not visited before, increment the count
+            views.visited = true;
+            views.count++;
+        }
+
         await views.save();
         res.status(200).send({ count: views.count });
     } catch (err) {
@@ -17,19 +29,18 @@ viewsController.post('/views', async (req, res) => {
     }
 });
 
-// Get total views count
+// GET request to fetch total view count
 viewsController.get('/views', async (req, res) => {
     try {
-        let views = await Views.findOne();
-        if (!views) {
-            return res.status(404).send('Views not found');
-        }
-        res.status(200).send({ count: views.count });
+        const views = await Views.aggregate([
+            { $group: { _id: null, totalCount: { $sum: "$count" } } }
+        ]);
+        const totalCount = views.length > 0 ? views[0].totalCount : 0;
+        res.status(200).send({ count: totalCount });
     } catch (err) {
         console.error(err.message);
         res.status(500).send('Server Error');
     }
 });
 
-
-module.exports = {viewsController};
+module.exports = { viewsController };

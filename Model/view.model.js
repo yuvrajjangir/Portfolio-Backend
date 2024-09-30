90059fa2cdbfedefdbd06dc1bd598ec83773a8b5
Yuvraj Jangir <136148186+yuvrajjangir@users.noreply.github.com>
2024-09-30 15:30:27 +0530

    UserStack API added

--- Model/view.model.js
+++ Model/view.model.js
@@ -1,15 +1,16 @@
-const mongoose = require("mongoose");
-
-const viewSchema = new mongoose.Schema({
-    count: { type: Number, default: 0 },
-    visitors: [
-        {
-            ip: { type: String, required: true },
-            device: { type: String, required: true }
-        }
-    ]
-});
-
-const Views = mongoose.model("Views", viewSchema);
-
-module.exports = { Views };
+ const mongoose = require("mongoose");
+ 
+ const viewSchema = new mongoose.Schema({
+     count: { type: Number, default: 0 },
+     visitors: [
+         {
+             ip: { type: String, required: true },
+             device: { type: String, required: true },
+             timestamp: { type: Date, default: Date.now }
+         }
+     ]
+ });
+ 
+ const Views = mongoose.model("Views", viewSchema);  // Make sure the collection is named correctly
+ 
+ module.exports = { Views };
